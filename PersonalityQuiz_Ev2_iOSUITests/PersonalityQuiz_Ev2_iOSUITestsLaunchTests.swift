7cb288440b4dc3110a26652dbd7404729b7f9d97
AngelMezaSchool <al03018750@tecmilenio.mx>
2024-04-15 23:21:29 -0600

    Initial Commit

---
+++ PersonalityQuiz_Ev2_iOSUITests/PersonalityQuiz_Ev2_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PersonalityQuiz_Ev2_iOSUITestsLaunchTests.swift
+//  PersonalityQuiz_Ev2_iOSUITests
+//
+//  Created by Angel Meza on 15/04/24.
+//
+
+import XCTest
+
+final class PersonalityQuiz_Ev2_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
